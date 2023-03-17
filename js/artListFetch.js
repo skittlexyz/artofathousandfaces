348b0c16cb0a8a1a6bed4b6a55cc78e70c8454c9
Moisés Corrêa <54242235+skittlexyz@users.noreply.github.com>
2023-03-17 17:26:06 -0300

    Add files via upload

--- js/artListFetch.js
+++ js/artListFetch.js
@@ -14,15 +14,9 @@ const artworkQuantity = (function() {
     for (let i = 0; i < data.length; i++) {
         quantity += (data[i].arts.at(-1).index) + 1;
     }
-    console.log("Artworks quantity: " + quantity)
     return quantity;
 })();
 const artworkPerColumn = (function() {
-    console.log("Artworks per column: " + [
-        ~~(artworkQuantity / 3) + ~~(artworkQuantity % 3), 
-        ~~(artworkQuantity / 3), 
-        ~~(artworkQuantity / 3)
-    ])
     return [
         ~~(artworkQuantity / 3) + ~~(artworkQuantity % 3), 
         ~~(artworkQuantity / 3), 
@@ -30,52 +24,43 @@ const artworkPerColumn = (function() {
     ];
 })();
 const artistPerColumn = (function() {
-    console.log("Artists per column: " + [
-        ~~(data.length / 3) + ~~(data.length % 3),
-        ~~(data.length / 3),
-        ~~(data.length / 3)
-    ]);
     return [
         ~~(data.length / 3) + ~~(data.length % 3),
         ~~(data.length / 3),
         ~~(data.length / 3)
     ];
 })();
+console.warn("Useful information below..." +
+    "\n   Artwork quantity: " + artworkQuantity +
+    "\n   Artwork per column: " + artworkPerColumn +
+    "\n   Artist per column: " + artistPerColumn
+);
+console.log("(╯°□°）╯︵ ┻━┻ odeio javascript!");
 
 // Create structure and append to DOM
 function applyChanges() {
     for (let x = 0; x < 3; x++) {
         for (let y = 0; y < artistPerColumn[x]; y++) {
-            for (let z = 0; y < data[(x*2)+y].arts.length; z++) {
-                console.log(data[(x*2)+y].arts);
+            for (let z = 0; z < data[(x*2)+y].arts.length; z++) {
+                let artCard = document.createElement('div');
+                let img = document.createElement('img');
+                let h1 = document.createElement('h1');
+                let p = document.createElement('p');
+                let path = "./assets/main-artworks/" + data[(x*2)+y].arts[z].src;
+                artCard.classList.add('art-card');
+                img.setAttribute('alt', 'arte');
+                img.setAttribute('src', path);
+                h1.innerText = data[(x*2)+y].arts[z].title;
+                p.innerText = data[(x*2)+y].artist;
+                artCard.appendChild(img);
+                artCard.appendChild(h1);
+                artCard.appendChild(p);
+                columns[x].appendChild(artCard);
+                if (z != data[(x*2)+y].arts.length - 1) {
+                    columns[x].appendChild(document.createElement('hr'));
+                }
             }
         }
     }
 }
-// applyChanges();
-// function applyChanges(index) {
-//     for (let x = 0; x < artworkPerColumn[index]; x++) {
-//         let artCard = document.createElement('div');
-//         let img = document.createElement('img');
-//         let h1 = document.createElement('h1');
-//         let p = document.createElement('p');
-//         let path = "./assets/main-artworks/" + data[index].arts[x].src;
-//         console.log("Index " + x + " path: " + "./assets/main-artworks/" + data[index].arts[x].src);
-//         artCard.classList.add('art-card');
-//         img.setAttribute('alt', 'arte');
-//         img.setAttribute('src', path);
-//         h1.innerText = data[index].arts[x].title;
-//         p.innerText = data[index].artist;
-//         artCard.appendChild(img);
-//         artCard.appendChild(h1);
-//         artCard.appendChild(p);
-//         columns[index].appendChild(artCard);
-//         if (x != artworkPerColumn[index] - 1) {
-//             columns[index].appendChild(document.createElement('hr'));
-//         }
-//     }
-// }
-console.log(data);
-// for (let i = 0; i < 3; i++) {
-//     applyChanges(i);
-// }
+applyChanges();
